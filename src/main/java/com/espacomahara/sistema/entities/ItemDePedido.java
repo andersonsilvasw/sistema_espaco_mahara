ce14cc8fd356f3480dccc5e9c2ee6d182ef637bf
andersonsilvasw <andersonsilvaedif@hotmail.com>
2023-10-07 19:10:53 -0300

    ItemDePedido, many-to-many association with extra attributes - PART 1

---
+++ src/main/java/com/espacomahara/sistema/entities/ItemDePedido.java
@@ -0,0 +1,82 @@
+package com.espacomahara.sistema.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.espacomahara.sistema.entities.pk.ItemDePedidoPK;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_item_de_pedido")
+public class ItemDePedido implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private ItemDePedidoPK id;
+	
+	private Integer quantidade;
+	private Double preco;
+	
+	public ItemDePedido() {
+	}
+
+	public ItemDePedido(Pedido pedido, Produto produto, Integer quantidade, Double preco) {
+		super();
+		id.setPedido(pedido);
+		id.setProduto(produto);
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+	
+	public Pedido getPedido() {
+		return id.getPedido();
+	}
+	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+	
+	public Produto getProduto() {
+		return id.getProduto();
+	}
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemDePedido other = (ItemDePedido) obj;
+		return Objects.equals(id, other.id);
+	}
+}
