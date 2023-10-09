c2a027d3ba8191138c931663432da9acbe9a72b5
andersonsilvasw <andersonsilvaedif@hotmail.com>
2023-10-09 18:04:33 -0300

    Usuario insert

--- src/main/java/com/espacomahara/sistema/services/UsuarioService.java
+++ src/main/java/com/espacomahara/sistema/services/UsuarioService.java
@@ -23,4 +23,8 @@ public class UsuarioService {
 		Optional<Usuario> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	public Usuario insert(Usuario obj) {
+		return repository.save(obj);
+	}
 }
